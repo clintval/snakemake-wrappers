1c2f85dbd65d507f7a66c6ede657555a87870bb7
clintval <valentine.clint@gmail.com>
2018-12-27 22:07:01 -0500

    Conda

--- recipe/bld.bat
+++
@@ -1,5 +0,0 @@
-"%PYTHON%" setup.py install
-
-if errorlevel 1 exit 1
-
-:: http://docs.continuum.io/conda/build.html for a list of environment variables that are set during the build process.
--- recipe/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-$PYTHON setup.py install
-
-# http://docs.continuum.io/conda/build.html for a list of environment variables that are set during the build process.
